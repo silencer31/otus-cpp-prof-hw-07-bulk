39ffad52faba2dcbc83c9e7f443d8cf754248bdc
Oleg M <moleg_9432@rambler.ru>
2023-02-18 16:59:27 +0300

    Add googletest module

--- run_input.h
+++ run_input.h
@@ -9,7 +9,7 @@ void run_commands_input(int number)
 {
 	std::shared_ptr<FileLogger>   file_logger_ptr = std::make_shared<FileLogger>();
 	std::shared_ptr<Collector>    collector_ptr   = std::make_shared<Collector>(file_logger_ptr, static_cast<size_t>(number));
-	std::shared_ptr<Interpreter>  interpreter_ptr = std::make_shared<Interpreter>(collector_ptr);
+	std::unique_ptr<Interpreter>  interpreter_ptr = std::make_unique<Interpreter>(collector_ptr);
 
 	// Проверка работы.
 
